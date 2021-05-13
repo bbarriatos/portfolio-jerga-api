1e28f9d9f1aee9b67faefd4a13f2df5ac9cf30dd
bbarriatos <bbarriatos@gmail.com>
2021-05-13 16:31:34 +0800

    Second commit

--- routes/portfolios.js
+++ routes/portfolios.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-// const { checkJwt } = require('../controllers/auth');
 const {
   getPortfolios,
   getPortfolioById,
   createPortfolio,
 } = require('../controllers/portfolios');
+const { checkJwt } = require('../controllers/auth');
 
 router.get('', getPortfolios);
 router.get('/:id', getPortfolioById);
 
-router.post('', createPortfolio);
+router.post('', checkJwt, createPortfolio);
 
 module.exports = router;
