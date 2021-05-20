fca6624691927350d32a5e2b432a95c4115cad6e
bbarriatos <bbarriatos@gmail.com>
2021-05-20 19:28:35 +0800

    error on geting the token url

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
+const request = require('request');
 const config = require('../config/dev');
 
 // Authentication middleware
@@ -27,4 +28,27 @@ exports.checkRole = (role) => (req, res, next) => {
       .status(401)
       .send('You are not authorized to access this resource!');
   }
+};
+
+exports.getAccessToken = (callback) => {
+  const options = {
+    methods: 'POST',
+    url: 'https://dev-sljrwyde.us.auth0.com/oauth/token',
+    headers: { 'content-type': 'application/json' },
+    form: {
+      grant_type: 'client_credentials',
+      client_id: 'hwc7BAGD9mjWbwXO8CgJX5D41symMeBz',
+      client_secret:
+        'xvxyD8PoSMh5CsT9hQwbRNAtdzTQmlWvz2uBBebYtWsgHLpvactKQYa0qFy6M6xG',
+      audience: 'https://dev-sljrwyde.us.auth0.com/api/v2/',
+    },
+  };
+
+  request(options, (error, res, body) => {
+    if (error) {
+      callback(error);
+    }
+
+    return callback(null, JSON.parse(body));
+  });
 };
