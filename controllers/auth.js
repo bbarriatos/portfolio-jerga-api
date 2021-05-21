04598bad939ae7ad127437d7a9d9ba8059af34ab
bbarriatos <bbarriatos@gmail.com>
2021-05-21 14:38:47 +0800

    UX Improvement done

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,7 +32,7 @@ exports.checkRole = (role) => (req, res, next) => {
 
 exports.getAccessToken = (callback) => {
   const options = {
-    methods: 'POST',
+    method: 'POST',
     url: 'https://dev-sljrwyde.us.auth0.com/oauth/token',
     headers: { 'content-type': 'application/json' },
     form: {
@@ -44,11 +44,31 @@ exports.getAccessToken = (callback) => {
     },
   };
 
-  request(options, (error, res, body) => {
-    if (error) {
-      callback(error);
-    }
+  return new Promise((resolve, reject) => {
+    request(options, (error, res, body) => {
+      if (error) {
+        return reject(new Error(error));
+      }
 
-    return callback(null, JSON.parse(body));
+      resolve(body ? JSON.parse(body) : '');
+    });
+  });
+};
+
+exports.getAuth0User = (accessToken) => (userId) => {
+  const options = {
+    method: 'GET',
+    url: `https://dev-sljrwyde.us.auth0.com/api/v2/users/${userId}?fields=name,picture,user_id`,
+    headers: { authorization: `Bearer ${accessToken}` },
+  };
+
+  return new Promise((resolve, reject) => {
+    request(options, (error, res, body) => {
+      if (error) {
+        return reject(new Error(error));
+      }
+
+      resolve(body ? JSON.parse(body) : '');
+    });
   });
 };
